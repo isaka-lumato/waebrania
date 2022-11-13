5c3f857bf891d91a22f46520fe729a8cf5aeb53d
isaka-lumato <isakawilly10@gmail.com>
2022-11-13 12:58:01 +0300

    Add navigation links animation and black overlay to the homepage image

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const ibada = [
-  {
+const ibada = [{
     image: 'https://static.wixstatic.com/media/bc97b991bed9d26c481b52c93e1e1ff0.jpg/v1/fill/w_370,h_300,fp_0.54_0.51,q_80,usm_0.66_1.00_0.01,enc_auto/bc97b991bed9d26c481b52c93e1e1ff0.jpg',
     title: 'JUMATANO',
     Description: 'Ibada hii ni bada kwa ajili ya ',
     time: '(1600hrs - 1800hrs)'
   },
-    {
-      image: 'https://static.wixstatic.com/media/bf2f08cf146747ad81361924a6d711c9.png/v1/fill/w_370,h_300,fp_0.49_0.42,q_85,usm_0.66_1.00_0.01,enc_auto/bf2f08cf146747ad81361924a6d711c9.png',
-      title: 'IJUMAA',
-      Description: 'Ibada hii ni bada kwa ajili ya ',
-      time: '(1600hrs - 1800hrs)'
-    }, {
-      image: 'https://static.wixstatic.com/media/5bb82d4a799849efa1a5cacda245f8fd.jpg/v1/fill/w_370,h_300,fp_0.50_0.50,q_80,usm_0.66_1.00_0.01,enc_auto/5bb82d4a799849efa1a5cacda245f8fd.jpg',
-      title: 'JUMAPILI',
-      Descriptiion: 'Lorem ipsum, dolor sit amet adipisicing elit. Deleniti, molestias.',
-      time: '(1600hrs - 1800hrs)',
-    },
-  ]
+  {
+    image: 'https://static.wixstatic.com/media/bf2f08cf146747ad81361924a6d711c9.png/v1/fill/w_370,h_300,fp_0.49_0.42,q_85,usm_0.66_1.00_0.01,enc_auto/bf2f08cf146747ad81361924a6d711c9.png',
+    title: 'IJUMAA',
+    Description: 'Ibada hii ni bada kwa ajili ya maombi ',
+    time: '(1600hrs - 1800hrs)'
+  }, {
+    image: 'https://static.wixstatic.com/media/5bb82d4a799849efa1a5cacda245f8fd.jpg/v1/fill/w_370,h_300,fp_0.50_0.50,q_80,usm_0.66_1.00_0.01,enc_auto/5bb82d4a799849efa1a5cacda245f8fd.jpg',
+    title: 'JUMAPILI',
+    Description: 'Ni siku ya ibada Kuu.',
+    time: '(1600hrs - 1800hrs)',
+  },
+]
 
 const ibadaUl = document.querySelector('.list-ibada');
 
@@ -37,7 +36,5 @@ ibada.forEach((ibada, index) => {
   ibadali.appendChild(desc);
   const time = document.createElement('p');
   time.innerHTML = ibada.time
-  ibadali.appendChild(time);  
-}
-)
-
+  ibadali.appendChild(time);
+})
